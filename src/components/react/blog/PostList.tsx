ec27f3359ab0484862852c21ff4ffaf55a064588
tantuyu <lionhuang914903@gmail.com>
2026-05-03 21:33:42 +0800

    chore: add motion on post list

--- src/components/react/blog/PostList.tsx
+++ src/components/react/blog/PostList.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/item";
 import { Badge } from "@/components/ui/badge";
 import RelativeTime from "@/components/react/RelativeTime";
+import { motion } from "motion/react";
 
 interface PostListProps {
   posts: BlogPost[];
@@ -27,7 +28,11 @@ export default function PostList({ posts }: PostListProps) {
             </div>
           )}
           <Item asChild size="sm" className="rounded-2xl">
-            <a href={`/blog/${post.id}`}>
+            <motion.a
+              href={`/blog/${post.id}`}
+              initial={{ opacity: 0, filter: "blur(10px)" }}
+              whileInView={{ opacity: 1, filter: "none" }}
+            >
               {post.heroImage && (
                 <img
                   src={post.heroImage}
@@ -81,7 +86,7 @@ export default function PostList({ posts }: PostListProps) {
                   <RelativeTime dateStr={post.pubDate} />
                 </span>
               </ItemFooter>
-            </a>
+            </motion.a>
           </Item>
         </div>
       ))}
